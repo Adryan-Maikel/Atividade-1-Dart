fe7fc943db586af09e3b1f4fbeeff1faddd433f7
Adryan-Maikel <100433470+Adryan-Maikel@users.noreply.github.com>
2023-02-02 02:37:42 -0300

    first commit

---
+++ main.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+import 'dart:math' as math;
+void main(){
+  choose();
+}
+
+choose(){
+  print("Digite o número do calculo desejado:");
+  print("1-Calcular a área de um círculo");
+  print("2-Calcular o salário de um funcionário");
+  print("3-Calcular o salário de um funcionário sobre suas vendas");
+  print("4-Calcular a 1ª Lei da termodinâmica");
+  print("5-Calcular a 2ª Lei da termodinâmica");
+  int inputNumber = int.parse(stdin.readLineSync()!);
+  if(inputNumber == 1){
+    calculationCircleArea();
+  }else if(inputNumber == 2){
+    workerSalaryCalculation();
+  }else if(inputNumber == 3){
+
+  }else if(inputNumber == 4){
+
+  }else if(inputNumber == 5){
+    
+  }else{
+    print("***********************");
+    print("Digite um valor válido!");
+    print("***********************");
+    chooseAgain();
+  }
+}
+
+chooseAgain(){
+  print("\nDeseja tentar de novo?\nS - sim ou N - não");
+  String? newChoose = stdin.readLineSync().toString().toUpperCase();
+  if(newChoose == "S"){
+    choose();
+  }
+  else{
+    return;
+  }
+}
+calculationCircleArea(){
+  print("Digite o valor do rario do círculo:");
+  double radius = double.parse(stdin.readLineSync()!);
+  double result = (radius * radius) * math.pi;
+  print("A area do circulo é $result");
+  chooseAgain();
+}
+workerSalaryCalculation(){
+  print("Digite o número de identificação do funcionário:");
+  int identificationNumber = int.parse(stdin.readLineSync()!);
+  print("Digite o número de horas trabalhadas:");
+  int workedHours = int.parse(stdin.readLineSync()!);
+  print("Digite valor que o funcionário recebe por horas trabalhadas:");
+  double hourlyWage = double.parse(stdin.readLineSync()!);
+  double salary = workedHours * hourlyWage;
+  print("\nCHAPA = $identificationNumber\nSALÁRIO = R\$ ${salary.toStringAsFixed(2)}");
+  chooseAgain();
+}
