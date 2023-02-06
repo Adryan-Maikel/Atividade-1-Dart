3549a8f2c9c02d316575d3eb264d510cba6847af
Adryan-Maikel <100433470+Adryan-Maikel@users.noreply.github.com>
2023-02-06 10:10:01 -0300

    Update

--- main.dart
+++ main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 import 'dart:math' as math;
+
 void main(){
   choose();
 }
@@ -19,9 +20,9 @@ choose(){
   }else if(inputNumber == 3){
     calculateCommission();
   }else if(inputNumber == 4){
-
+    firstCalculatationThermodynamics();
   }else if(inputNumber == 5){
-    
+    secondCalculatationThermodynamics();
   }else{
     print("***********************");
     print("Digite um valor válido!");
@@ -40,13 +41,15 @@ chooseAgain(){
     return;
   }
 }
+
 calculationCircleArea(){
-  print("Digite o valor do raio do círculo:");
+  print("Digite o valor do rario do círculo:");
   double radius = double.parse(stdin.readLineSync()!);
   double result = (radius * radius) * math.pi;
   print("A area do circulo é $result");
   chooseAgain();
 }
+
 workerSalaryCalculation(){
   print("Digite o número de identificação do funcionário:");
   int identificationNumber = int.parse(stdin.readLineSync()!);
@@ -58,6 +61,7 @@ workerSalaryCalculation(){
   print("\nCHAPA = $identificationNumber\nSALÁRIO = R\$ ${salary.toStringAsFixed(2)}");
   chooseAgain();
 }
+
 calculateCommission(){
   print("Digite o nome do vendedor:");
   String name = stdin.readLineSync()!;
@@ -68,4 +72,35 @@ calculateCommission(){
   double finalSalary = salary + sales * 0.15;
   print("$name receberá R\$ ${finalSalary.toStringAsFixed(2)} esse mês.");
   chooseAgain();
+}
+
+firstCalculatationThermodynamics(){
+  print("Fórmula:\nQ = T + ΔU");
+  print("Qual a quantidade de calor recebida em Joule?");
+  int q = int.parse(stdin.readLineSync()!);
+  print("Qual a temperatura em Joule que o gás realiza o trabalho?");
+  int t = int.parse(stdin.readLineSync()!);
+  print("Qual era a energia interna em Joule do sistema antes de receber o calor?");
+  int u = int.parse(stdin.readLineSync()!);
+  print("$q = $t + (U - $u)");
+  print("$q = $t - $u + U");
+  int result = u - t + q;
+  print("U = $result J");
+  chooseAgain();
+}
+
+secondCalculatationThermodynamics(){
+  print("Fórmula:\nN = T/Q1 * 100%");
+  print("Um motor à vapor realiza um trabalho em quantos quilojoules?");
+  int t = int.parse(stdin.readLineSync()!);
+  t *= 1000;
+  print("Qual a quantidade de calor que lhe é fornecido em quilojoules?");
+  int q = int.parse(stdin.readLineSync()!);
+  q *= 1000;
+  print("N = $t/$q * 100%");
+  double result = t / q;
+  print("N = ${result.toStringAsFixed(2)} * 100%");
+  result *= 100;
+  print("N = ${result.toStringAsFixed(2)}%");
+  chooseAgain(); 
 }
